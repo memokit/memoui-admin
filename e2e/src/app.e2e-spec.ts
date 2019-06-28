7d2cee4f662f6067f5bda4e4930ae55112fabb26
Jeki Kit <kit.jrs@gmail.com>
2019-06-28 15:13:10 +0700

    upgrade to ng 8

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to memoui!');
+    expect(page.getTitleText()).toEqual('Welcome to memoui-admin!');
   });
 
   afterEach(async () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,65 +1,30 @@
-import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule, Inject, APP_ID, PLATFORM_ID } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app.routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgZorroAntdModule, NZ_ICON_DEFAULT_TWOTONE_COLOR, NZ_ICONS, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { IconDefinition } from '@ant-design/icons-angular';
-import { AccountBookFill, AlertFill, AlertOutline } from '@ant-design/icons-angular/icons';
-import en from '@angular/common/locales/en';
+import { NgZorroAntdModule, NZ_I18N, th_TH } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { isPlatformBrowser } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import th from '@angular/common/locales/th';
 
-const icons: IconDefinition[] = [ AccountBookFill, AlertOutline, AlertFill ];
-
-export class HammerConfig extends HammerGestureConfig {
-  overrides = <any>{
-    swipe: { velocity: 0.4, threshold: 20 }
-  };
-}
-
-const APP_PROVIDERS = [
-  // AppState,
-  // GlobalState,
-  // Title,
-  // CookieService,
-  // {
-  //   provide: HTTP_INTERCEPTORS,
-  //   useClass: TokenInterceptor,
-  //   multi: true
-  // },
-  // { provide: LocationStrategy, useClass: HashLocationStrategy },
-  // { provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig }
-  { provide: NZ_I18N, useValue: en_US },
-  { provide: NZ_ICON_DEFAULT_TWOTONE_COLOR, useValue: '#00ff00' },
-  { provide: NZ_ICONS, useValue: icons },
-  { provide: HAMMER_GESTURE_CONFIG, useClass: HammerConfig }
-];
-
-
+registerLocaleData(th);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: "memoui" }),
-    BrowserAnimationsModule,
-    HttpClientModule,
+    BrowserModule,
     AppRoutingModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [APP_PROVIDERS],
+  providers: [{ provide: NZ_I18N, useValue: th_TH }],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    @Inject(APP_ID) private appId: string
-  ) {
-    const platform = isPlatformBrowser(platformId)
-      ? "in the browser" : "on the server";
-    console.log(`Running ${platform} with appId=${appId}`);
-  }
-}
+export class AppModule { }
